f7a6cf6751ebde9c7646e24f5b758536790c1ab9
quietlycreeping <60121681+quietlycreeping@users.noreply.github.com>
2025-04-01 17:01:30 -0400

    que

--- Assets/Scripts/Managers/QueManager.cs
+++ Assets/Scripts/Managers/QueManager.cs
@@ -10,9 +10,23 @@ public class queManager : MonoBehaviour
     [Tooltip("List of customer gameObjects")]
     public List<GameObject> customers = new List<GameObject>();
 
+    GameObject[] activeCustomers;
+
+//================= Core Functions =================//
     // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        GameObject[] activeCustomers = new GameObject[5];
+    }
+
+//================= Functions =================//
+    void updateActiveQue(List<GameObject> customers)
+    {
+        int index = 0;
+
+        while (activeCustomers[index] == null)
+        {
+            index ++;
+        }
     }
 }
